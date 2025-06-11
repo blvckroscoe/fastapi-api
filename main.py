373641eac5b3cbd6391298bae8a713902ffa2e93
blvckroscoe <blvckroscoe@gmail.com>
2025-06-11 14:48:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from fastapi import FastAPI
 from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Привет, братец! API работает 🔥"}
+
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"Ассаламу алейкум, {name}!"}
 
 class Message(BaseModel):
     user: str
